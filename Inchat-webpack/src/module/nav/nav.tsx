cc6c7b8962954bb65e38c9c35e70e51a779c602e
ghc <1257075652@qq.com>
2018-03-24 12:45:11 +0800

    update

--- Inchat-webpack/src/module/nav/nav.tsx
+++ Inchat-webpack/src/module/nav/nav.tsx
@@ -19,7 +19,7 @@ export default class Nav extends React.Component < initProps > {
             <div className="nav">
                 <div className="max-width nav-box">
                     <span className="logo">Inchat</span>
-                    <span className="user-entry">欢迎您，请先<a className="link" href="../../register.html">注册</a>/ <a className="link" onClick={this.showLogin.bind(this)}>登录</a> </span>
+                    <span className="user-entry">欢迎您，请先<a className="link" onClick={this.toRegister.bind(this)}>注册</a>/ <a className="link" onClick={this.showLogin.bind(this)}>登录</a> </span>
                     <Login show={this.state.showModal}  normalOpen={true} />
                 </div>
             </div>
@@ -29,5 +29,13 @@ export default class Nav extends React.Component < initProps > {
         this.setState({
             showModal: true
         });
+    }
+    /**
+     * @description 转到注册页面
+     */
+    toRegister() {
+        const href = window.location.href;
+
+        window.location.href = href.substr(0, href.lastIndexOf("/") + 1) + "register.html";
     }
 }
